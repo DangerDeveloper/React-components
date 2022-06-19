5018acde59067a105cdab0cf299136694893d6a4
Ajay <ajaysingh56656@gmail.com>
2022-06-19 14:47:06 +0530

    After Side Circle

--- src/components/inside/inside.com.jsx
+++ src/components/inside/inside.com.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Profile from '../profile/profile.com';
 import './inside.sty.scss'
 
--- src/components/main/main.com.jsx
+++ src/components/main/main.com.jsx
@@ -6,57 +6,67 @@ import { useEffect } from 'react';
 
 function Main() {
 
-    // useEffect(() => {
-    //     function handleResize() {
-    //         document.querySelectorAll('.main-circle').forEach(ciclegraph => {
-    //             let innerCircle = ciclegraph.querySelector('.inside-circle');
-    //             console.log(innerCircle.clientWidth);
-    //             let circles = ciclegraph.querySelectorAll('.single-date');
-    //             let angle = 360-25, dangle = 345 / circles.length;
-    //             for( let i = 0; i < circles.length; ++i ){
-    //                 let circle = circles[i];
-    //                 angle += dangle;
-    //                 circle.style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth / 2.4}px) rotate(-${angle}deg)`
-    //             }
-    //         })
-    //     }
-    //     window.addEventListener("resize", handleResize);
-    //     handleResize();
-    //     return () => window.removeEventListener("resize", handleResize);
-    //   }, []);
-
     useEffect(() => {
-        document.querySelectorAll('.challenge-circle-main-circle').forEach(ciclegraph => {
-            // let innerCircle = ciclegraph.querySelector('.challenge-circle-inside-circle');
-            // console.log(innerCircle.clientWidth);
-            let circles = ciclegraph.querySelectorAll('.challenge-circle-single-date');
-            let semiCircle = ciclegraph.querySelector('.semi-circle');
-            circles = [...circles, semiCircle];
-            // console.log(circles);
-            let angle = 360-25, dangle = 355 / circles.length;
-            let circle;
-            for( let i = 0; i < circles.length; ++i ){
-                circle = circles[i];
-                angle += dangle;
-                circle.style.transform = `rotate(${angle}deg) translate(30vw) rotate(-${angle}deg)`; 
-            }
-            circle.style.transform = `rotate(${angle}deg) translate(30vw) rotate(-${angle - 145}deg)`;
-        })
+        function handleResize() {
+            let semiCircle;
+            document.querySelectorAll('.challenge-circle-main-circle').forEach(ciclegraph => {
+                let circles = ciclegraph.querySelectorAll('.challenge-circle-single-date');
+                semiCircle = ciclegraph.querySelector('.challenge-circle-semi-circle');
+                circles = [...circles, semiCircle];
+                let angle = 360-25, dangle = 357 / circles.length;
+                let circle;
+                for( let i = 0; i < circles.length; ++i ){
+                    circle = circles[i];
+                    angle += dangle;
+                    circle.style.transform = `rotate(${angle}deg) translate(${ciclegraph.clientWidth / 2}px) rotate(-${angle}deg)`
+                }
+                circle.style.transform = `rotate(${angle - 2}deg) translate(${ciclegraph.clientWidth / 2}px) rotate(-${angle - 149}deg)`
+            })
+
+            let outerCircle = document.querySelector('.challenge-circle-main-circle-outer');
+            let innerCircle = document.querySelector('.challenge-circle-inside-circle');
+            innerCircle.style.width = outerCircle.clientWidth * 0.7 + 'px';
+            semiCircle.style.width = (((outerCircle.clientWidth - innerCircle.clientWidth) / 2) + 1) + 'px';
+        }
+        window.addEventListener("resize", handleResize);
+        handleResize();
+        return () => window.removeEventListener("resize", handleResize);
       }, []);
+
+    // useEffect(() => {
+    //     document.querySelectorAll('.challenge-circle-main-circle').forEach(ciclegraph => {
+    //         // let innerCircle = ciclegraph.querySelector('.challenge-circle-inside-circle');
+    //         // console.log(innerCircle.clientWidth);
+    //         let circles = ciclegraph.querySelectorAll('.challenge-circle-single-date');
+    //         let semiCircle = ciclegraph.querySelector('.challenge-circle-semi-circle');
+    //         circles = [...circles, semiCircle];
+    //         // console.log(circles);
+    //         let angle = 360-25, dangle = 357 / circles.length;
+    //         let circle;
+    //         for( let i = 0; i < circles.length; ++i ){
+    //             circle = circles[i];
+    //             angle += dangle;
+    //             circle.style.transform = `rotate(${angle}deg) translate(30vw) rotate(-${angle}deg)`; 
+    //         }
+    //         // circle.style.transform = `rotate(${angle - 180}deg) rotate(-${angle - 180}deg)`;
+    //     })
+    //   }, []);
 
     const dateList =  daysInMonth();
     const afterAddingDateList = [...dateList, []];
 
     // console.log(afterAddingDateList[-1]);
-    return (<div className="challenge-circle-main-circle">
-            <Inside dateList={dateList}/>
-            {afterAddingDateList.map((date, key) => {
-                if(!date[0]) {
-                    return <div key={key} className="semi-circle">
-                    </div>
-                }
-                return <Date key={key} date={date}/>
-            })}
+    return (<div className="challenge-circle-main-circle-outer">
+            <div className='challenge-circle-main-circle'>
+                <Inside dateList={dateList}/>
+                {afterAddingDateList.map((date, key) => {
+                    if(!date[0]) {
+                        return <div key={key} className="challenge-circle-semi-circle">
+                        </div>
+                    }
+                    return <Date key={key} date={date}/>
+                })}
+            </div>
         </div>);
 }
 
